0f5c451b90b8b6622ca754c97e7ee18e5a8a77de
Chan Thongjerm <48668178+9chanz@users.noreply.github.com>
2026-06-12 02:33:50 +0700

    Initial

---
+++ index.js
@@ -0,0 +1,42 @@
+import {
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  readFileSync,
+  rmSync,
+  writeFileSync
+} from 'fs'
+
+/**
+ * A mapping of source directory (name) to output directory for generated Astro components.
+ * @typedef {{ [key: string]: string }} DirMap
+ */
+
+/** @type {DirMap} */
+const dirMap = {
+  icons: 'icons'
+}
+
+const componentScript = `---
+import type { HTMLAttributes } from 'astro/types'
+
+export type Props = HTMLAttributes<'svg'>
+---
+`
+
+Object.keys(dirMap).forEach((dir) => {
+  const files = readdirSync(`./node_modules/bootstrap-icons/${dir}`)
+  const outDir = `./${dirMap[dir]}`
+
+  if (existsSync(outDir)) {
+    rmSync(outDir, { recursive: true })
+  }
+  mkdirSync(outDir, { recursive: true })
+
+  files.forEach((file) => {
+    const fileName = file.replace('.svg', '')
+    const svgElement = readFileSync(`./node_modules/bootstrap-icons/${dir}/${file}`, 'utf-8')
+    const svgIcon = svgElement.replace(/class="[^"]*"/, '{...Astro.props}')
+    writeFileSync(`${outDir}/${fileName}.astro`, `${componentScript}\n${svgIcon}\n`)
+  })
+})
