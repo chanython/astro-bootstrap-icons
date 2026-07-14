9e1a11dc8b843d4a5431931c2dbc88ec7cfc4741
Chan Thongjerm <48668178+9chanz@users.noreply.github.com>
2026-07-15 02:32:36 +0700

    Refactor props handling

--- src/index.ts
+++ src/index.ts
@@ -22,16 +22,15 @@ Object.keys(dirMap).forEach((dir) => {
     const svgElement = readFileSync(`./node_modules/bootstrap-icons/${dir}/${file}`, 'utf-8')
     const propsScript = readFileSync('./src/components/props.astro', 'utf-8')
 
-    const removeAllAttributes = svgElement.replace(/<svg([^>]*)>/, '<svg>')
-    const xmlnsAttr = removeAllAttributes.replace(
-      /<svg([^>]*)>/,
-      `<svg$1 xmlns={xmlns ?? "http://www.w3.org/2000/svg"}>`,
-    )
+    const removeAllAttrs = svgElement.replace(/<svg([^>]*)>/, '<svg>')
+    const classAttr = removeAllAttrs.replace(/<svg([^>]*)>/, `<svg$1 class={className}>`)
+    const restProps = classAttr.replace(/<svg([^>]*)>/, `<svg$1 {...rest}>`)
+    const xmlnsAttr = restProps.replace(/<svg([^>]*)>/, `<svg$1 xmlns={xmlns ?? "http://www.w3.org/2000/svg"}>`)
     const widthAttr = xmlnsAttr.replace(/<svg([^>]*)>/, `<svg$1 width={width ?? 16}>`)
     const heightAttr = widthAttr.replace(/<svg([^>]*)>/, `<svg$1 height={height ?? 16}>`)
     const fillAttr = heightAttr.replace(/<svg([^>]*)>/, `<svg$1 fill={fill ?? "currentColor"}>`)
     const viewBoxAttr = fillAttr.replace(/<svg([^>]*)>/, `<svg$1 viewBox={viewBox ?? "0 0 16 16"}>`)
-    const svgIcon = viewBoxAttr.replace(/<svg([^>]*)>/, `<svg {...props}$1>`)
+    const svgIcon = viewBoxAttr
 
     writeFileSync(`${outDir}/${fileName}.astro`, `${propsScript}\n${svgIcon}\n`)
   })
